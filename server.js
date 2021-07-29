ada662cf048e20c4251f7db3b5523437b8370aff
muhammadAdnan01 <88036342+muhammadAdnan01@users.noreply.github.com>
2021-07-29 15:16:09 +0500

    server file updated

--- server.js
+++ server.js
@@ -3,32 +3,32 @@ app = express(),
 port = process.env.PORT || 3000;
 const {Client} = require('pg');
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize('todo_database', 'postgres', '1234');
+// var sequelize = new Sequelize('todo_database', 'postgres', '1234');
 
-var User = sequelize.define('todo', {
-    todo_id: Sequelize.INTEGER,
-    description: Sequelize.STRING
-  });
+// var User = sequelize.define('todo', {
+//     todo_id: Sequelize.INTEGER,
+//     description: Sequelize.STRING
+//   });
   
-  sequelize.sync().then(function() {
-    return User.create({
-        todo_id: 123,
-        description: 'Hello world'
-    });
-  }).then(function(jane) {
-    console.log('got jane here =====', jane.get({
-      plain: true
-    }));
-  });
-// const client = new Client({
-//     host : 'localhost',
-//     user : 'postgres',
-//     password : '1234',
-//     port : 5432,
-//     database : 'todo_database'
-// })
+//   sequelize.sync().then(function() {
+//     return User.create({
+//         todo_id: 123,
+//         description: 'Hello world'
+//     });
+//   }).then(function(jane) {
+//     console.log('got jane here =====', jane.get({
+//       plain: true
+//     }));
+//   });
+const client = new Client({
+    host : 'localhost',
+    user : 'postgres',
+    password : '1234',
+    port : 5432,
+    database : 'todo_database'
+})
 
-// client.connect().then(() => console.log("connected")).catch(err => console.log("got error =====>" , err))
+client.connect().then(() => console.log("connected")).catch(err => console.log("got error =====>" , err))
 app.get('/' , function(req , res){
 
     console.log("api Called");
