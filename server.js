ead36d40882ebe9b11bef58497d499cf10989783
aliTanveerInvoZone <ali.tanveer@invozone.com>
2021-07-30 10:33:30 +0500

    server.js Updated

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
 const port = process.env.PORT || 3000;
 const swaggerJsdoc = require('swagger-jsdoc');
-const { Client } = require('pg');
-const Sequelize = require('sequelize');
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 const options = {
   definition: {
@@ -18,30 +25,18 @@ const options = {
   apis: ['server.js'], // files containing annotations as above
 };
 
-/**
- * @openapi
- * /:
- *   get:
- *     description: Welcome to swagger-jsdoc!
- *     responses:
- *       200:
- *         description: Returns a mysterious string.
- */
-
-const openapiSpecification = swaggerJsdoc(options);
+const openApiSpecification = swaggerJsdoc(options);
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
+// -------------- APP ROUTES ------- //
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openApiSpecification));
 app.use('/auth', require('./Routes/Auth'));
 app.use('/activities', require('./Routes/Activities'));
 app.use('/heartRate', require('./Routes/HeartRate'));
 app.use('/Weight', require('./Routes/Weight'));
 app.use('/SleepDuration', require('./Routes/SleepDuration'));
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
-// });
 app.listen(port);
 console.log('app is listening at port', port);
